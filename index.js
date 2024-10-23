4ae7d483b1a38f7e849a3db84339c0f6c99e607f
Turkiano <turki.saeed.ksa@gmail.com>
2024-10-23 08:36:49 +0300

    trying to uderstand DOM

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const toDos = ["Study DOM", "Study JavaScript", "Workout"];
-const container = document.querySelector(".container"); //to target the div class in HTML file
-const ul = document.createElement("ul");
-ul.classList.add("todos");
-container.appendChild(ul); //to include uordered list into the container
+const form = document.getElementById("#add-todo");
 
-toDos.forEach((Element) => {
-  const li = document.createElement("li");
-  li.classList.add("toDo");
-  li.textContent = Element; // Set the text content of each list item
-  ul.appendChild(li); // Append each list item to the unordered list
+console.log(toDos);
+
+function renderTodos() {
+  const container = document.querySelector(".container"); //to target the div class in HTML file
+  const ul = document.createElement("ul");
+  ul.classList.add("todos");
+  container.appendChild(ul); //to include uordered list into the container
+
+  toDos.forEach((Element) => {
+    const li = document.createElement("li");
+    li.classList.add("toDo");
+    li.textContent = Element; // Set the text content of each list item
+    ul.appendChild(li); // Append each list item to the unordered list
+  });
+}
+window.addEventListener("load", () => {
+  renderTodos();
 });
 
-const form = document.getElementById("#add-todo");
-form.addEventListener("submit", (Element) => {
-  const value = document.querySelector(".toDo-input").value;
+function addTodo(value) {
   const ul = document.querySelector(".todos");
   const li = document.createElement("li");
   li.classList.add("toDo");
   li.textContent = value;
   ul.appendChild(li);
+}
+
+form.addEventListener("submit", (Element) => {
+  const value = document.querySelector(".toDo-input").value;
+  addTodo(value);
   Element.preventDefault();
 });
