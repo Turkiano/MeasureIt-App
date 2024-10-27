3dff55241596375ed276c5419fddc61bd688471e
Turkiano <turki.saeed.ksa@gmail.com>
2024-10-27 09:44:37 +0300

    exersice one - finito - variable

--- index.js
+++ index.js
@@ -8,7 +8,35 @@
     and greeting message.
  */
 
+//camel Case
+const userName = "Marriah";
+//all caps
+const ROLE = "admin";
+//pascal Case
+const UserName = "Walter";
 
-    //a. Declare a variable 'name', and assign it to a string value.
+//snake or kebab Case
+const user_name = "SKy";
 
+//a. Declare a variable 'name', and assign it to a string value.
+const name = "Turkiano";
 
+//b. Declare a variable 'age', and assign it to a number value.
+const age = 18;
+
+//c. Declare a variable 'isStudent', and assign it to a boolean value.
+const isStudent = true;
+
+//d. Declare a variable 'result', and assign it to a method adding two numbers.
+const numberOne = 10;
+const numberTwo = 7;
+const result = numberOne + numberTwo;
+console.log(result);
+
+//e.Declare a variable 'message ', and assign it to a string concatenation of name, and greeting message.
+const message = "hello" + userName;
+
+console.log(message);
+
+const exampleTwo = `Hello, ${userName}`;
+console.log(exampleTwo);
