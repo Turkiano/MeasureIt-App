4a28f3c2842d513faf720e08be9a6ab2b49174b5
Turkiano <turki.saeed.ksa@gmail.com>
2024-10-27 11:22:27 +0300

    working on loops

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const UserName = "Walter";
 //snake or kebab Case
 const user_name = "SKy";
 
+/**
+ * ----- Exercise 01. Variables & Data Types: --------------------
+ */
+
 //a. Declare a variable 'name', and assign it to a string value.
 const name = "Turkiano";
 
@@ -40,12 +44,14 @@ console.log(message);
 
 const exampleTwo = `Hello, ${userName}`;
 console.log(exampleTwo);
+console.log("------------------------------------------");
+
 /**
- * --------------------------------------------------
+ * ------------Exercise 02.Conditonal Statements:----------------------------
  */
 
 /**
-  Exercise 02.Conditonal Statements:
+ 
   a. Write an if statement to check if a number is greater than 10.
   b. Write an if-else statement to check if a number is even or odd.
   c. write nested if statements to check if a student
@@ -63,10 +69,80 @@ if (age > 10) {
 }
 
 //b. Write an if-else statement to check if a number is even or odd.
-const number = 345;
+const number = 20;
 if (number % 2 === 0) {
-  console.log("This number " + number + "is even");
+  console.log("This number " + number + " is even");
+} else if (number % 1 === 0) {
+  console.log("This number is odd");
 }
-return "This number is odd";
+//another way to do it
+const isEven = number % 2 === 0;
+const isOdd = number & (1 === 0);
+if (isEven) {
+  console.log("Example 02: This number is even");
+}
+if (isOdd) {
+  console.log("Example 02: This number is odd");
+}
 
 //  c. write nested if statements to check if a student
+const score = 50;
+if (score >= 90) {
+  console.log(" You got A");
+} else if (score >= 80) {
+  console.log(" You got B");
+} else if (score >= 60) {
+  console.log(" You got C");
+} else {
+  console.log(" You got F, do it again");
+}
+
+//d. write a switch statement to display a message,
+function whatDay(day) {
+  switch (day) {
+    case 1:
+      return "Sunday";
+
+    case 2:
+      return "Monday";
+
+    default:
+      return "pick one of the two days above";
+  }
+}
+console.log(whatDay(1));
+console.log(whatDay(2));
+console.log(whatDay(6));
+
+
+
+/*
+e. write an ternary operator to determine if a person
+  is eligible to vote based on their age.
+*/
+const herAge = 28
+age > 18 
+?console.log("yes, you can drive")
+:console.log("come back, when you grow up")
+
+//it is the same as the if statement below:
+if(herAge > 18){
+  console.log("yes, you can drive")
+} else {
+  console.log("come back, when you grow up")
+}
+console.log("------------------------------------------");
+
+/**
+ * ------------Exercise 03.Loops:----------------------------
+ 
+a. Write a for loop to iterate from 1 to 5 and display each number.
+b. Write a while loop to print even numbers between 1 and 10.
+c. Write a do-while loop to prompt the user to enter a number until they enter a negative number.
+d. Iterate over an array of names using a for loop and display each name.
+e. Use a for loop to find the sum of all numbers in an array.
+
+*/
+
+
+
