10519c85a4b0d227557ce395e3fff734b842330d
Turkiano <turki.saeed.ksa@gmail.com>
2024-10-31 11:38:30 +0300

    lost with the delete button

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //global variables
-const toDos = ["Study DOM", "Study JavaScript", "Workout"];
+let toDos = ["Study DOM", "Study JavaScript", "Workout"];
 const form = document.getElementById("add-todo");
+const ul = document.createElement("ul");
 
 //to render input values
 function renderTodos(array) {
   const container = document.querySelector(".container"); //to target the div class in HTML file
-  const ul = document.createElement("ul");
   ul.classList.add("todos");
   container.appendChild(ul); //to include uordered list into the container
 
@@ -13,59 +13,68 @@ function renderTodos(array) {
   array.forEach((task) => {
     const li = document.createElement("li");
     const span = document.createElement("span"); //1. Declaring the [span] tag
-
+    renderDeleteBtn();
     li.classList.add("toDo");
 
     span.textContent = task; //  2.targeting all the task to be inside the span tag.
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", (e) => {
-      const selectedToDo = e.target.previousSibling.textContent;
-      removeToDo(selectedToDo);
-    });
+
     li.appendChild(span); //3. Append span into list
     li.appendChild(deleteBtn);
     ul.appendChild(li); // Append each list item to the unordered list
   });
 }
 
-//to render input values after loading
-window.addEventListener("load", () => {
-  renderTodos();
+//to render taks, every time we load
+window.addEventListener("load", (e) => {
+  renderTodos(toDos);
 });
+//to render deleteBtn, every time we add a task
 
+function renderDeleteBtn() {
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", (e) => {
+    const selectedToDo = e.target.previousSibling.textContent;
+    removeToDo(selectedToDo);
+  });
+}
 //to add new values to the list
 function addTodo(value) {
   const ul = document.querySelector(".todos");
   const li = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", (e) => {
+    const selectedToDo = e.target.previousSibling.textContent;
+    removeToDo(selectedToDo);
+  });
+
   toDos.push(value);
   console.log(toDos); //to test the items in the main Array
 
   li.classList.add("toDo");
   li.textContent = value;
+  li.appendChild(deleteBtn);
   ul.appendChild(li);
 }
 
 //to remove an item from the list
 function removeToDo(selectedToDo) {
-  console.log("The selected Task: " + selectedToDo);
-  const filteredToDo = toDos.filter((todo) => {
-    return todo !== selectedToDo;
+  // console.log("The selected Task: " + selectedToDo);
+  const filteredToDo = toDos.filter((toDo) => {
+    return toDo !== selectedToDo;
   });
-  console.log("The filteredToDo : " + filteredToDo);
-  console.log("The toDos : " + toDos);
+
+  ul.innerHTML = ""; //to remove the deleted task
+  toDos = filteredToDo; //to updated the tasks
 
   renderTodos(filteredToDo); //to render the list after removing
 }
 
-//to capture the new value
+//to capture the new value, when submitting
 form.addEventListener("submit", (Element) => {
   const value = document.querySelector(".toDo-input").value;
   addTodo(value);
   Element.preventDefault();
-});
-
-
-window.addEventListener("load", (e) => {
-  renderTodos(toDos);
 });
