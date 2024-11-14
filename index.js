cfd9d38bf28aba41c5031db29112f29ba34827fd
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-14 11:17:57 +0300

    done with encapsulation with getter & setter

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-class person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+function person(name, age) {
+  let _name = name; //private variable
+  let _age = age;
+
+  //public methods (getter & Setter)
+  this.getName = function () {
+    return _name;
+  };
+
+  this.setName = function (name) {
+    _name = name;
+  };
+
+  this.getAge = function () {
+    return _age;
+  };
+
+  this.setAge = function (age) {
+    if (age >= 0) _age = age;
+  };
 }
 
-const person1 = new person("Turkiano", 38);
-const person2 = new person("Turkiano", 38);
-
-console.log(`the name is: ` + person1.name);
+//A new object
+const person1 = new person("Turki", 38);
+console.log("ex 1: " + person1.getName());
+person1.setName("Khalid");
+console.log("ex 2: " + person1.getName());
