f436bca9d6719d5fc750b3a72bb80bc1c1f214f9
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-14 11:42:30 +0300

    done with encapsulation using the constructor

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-function person(name, age) {
-  let _name = name; //private variable
-  let _age = age;
+class Person {
+  #name; //private field
+  #age;
 
-  //public methods (getter & Setter)
-  this.getName = function () {
-    return _name;
-  };
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
 
-  this.setName = function (name) {
-    _name = name;
-  };
+  //public methods
+  getName() {return this.#name;}
 
-  this.getAge = function () {
-    return _age;
-  };
+  setName(name) {this.#name = name;}
 
-  this.setAge = function (age) {
-    if (age >= 0) _age = age;
-  };
+  getAge() {return this.#age;}
+
+  setAge(age) {if (age >= 0) this.#age = age;}
 }
 
-//A new object
-const person1 = new person("Turki", 38);
-console.log("ex 1: " + person1.getName());
-person1.setName("Khalid");
-console.log("ex 2: " + person1.getName());
+//new objects
+const personOne = new Person("Khalid");
+console.log("Ex 01: " + personOne.getName());
+
+personOne.setName("Turkiano");
+console.log("Ex 02: " + personOne.getName());
