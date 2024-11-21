680c9528e9835a4791d2e34cdd463bf6b795d0a7
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-21 14:26:59 +0300

    Create, Read, and delete - finito

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-
-let theList = [1,2,3];
+let theList = [1, 2, 3];
 let newList = 4;
-let updatedList = addItemToArray(theList, newList);//invoking 1st function
+let updatedList = addItemToArray(theList, newList); //invoking 1st function
 
-console.log("This is for testing: " + updatedList);
+console.log("01.testing addItem: " + updatedList);
 renderList(updatedList); //invoking 2nd function
+
+removeItemFromArray(updatedList, 10);
+console.log("03.testing removed itemd: " + updatedList);
 
 
 //1.add items to the list
@@ -16,16 +18,15 @@ function addItemToArray(arr, item) {
 //2.render the List
 function renderList(arrai) {
   arrai.forEach((items) => {
-    console.log("rendering Item: " + items);
+    console.log("02.rendering Item: " + items);
   });
 }
 
 //3. Deleting items from the list
-function removeItemFromArray(array, index){
-if (index >=0 && index < array.length){
-  array.splice(index, 1)
-}
-else {
-  console.log("invalid index: " + index)
-}
+function removeItemFromArray(array, index) {
+  if (index >= 0 && index < array.length) {
+    array.splice(index, 1);
+  } else {
+    console.log("invalid index: " + index);
+  }
 }
