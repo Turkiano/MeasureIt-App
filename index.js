6f29ee85f9afd9ab4ece52d29ebb2f407eb7e2ef
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-21 14:43:55 +0300

    CRUD function is finished

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 let theList = [1, 2, 3];
-let newList = 4;
-let updatedList = addItemToArray(theList, newList); //invoking 1st function
+let newItem = 4;
+let updatedList = addItemToArray(theList, newItem); //invoking 1st function
 
-console.log("01.testing addItem: " + updatedList);
+console.log("01.testing Add function: " + updatedList);
 renderList(updatedList); //invoking 2nd function
 
-removeItemFromArray(updatedList, 10);
-console.log("03.testing removed itemd: " + updatedList);
+removeItemFromArray(updatedList, 2); //invoking 3rd function
+console.log("03.testing removed function: " + updatedList);
 
+editItemInArray(updatedList, 5); //invoking 4th function
+console.log("04.testing Edit function: " + updatedList);
 
-//1.add items to the list
+countItemsInArray(updatedList);
+console.log("05.testing Counter function: " + updatedList);
+
+//1.add function
 function addItemToArray(arr, item) {
   arr.push(item);
   return arr;
 }
 
-//2.render the List
+//2.render function
 function renderList(arrai) {
   arrai.forEach((items) => {
     console.log("02.rendering Item: " + items);
   });
 }
 
-//3. Deleting items from the list
+//3. Delete function
 function removeItemFromArray(array, index) {
   if (index >= 0 && index < array.length) {
     array.splice(index, 1);
   } else {
     console.log("invalid index: " + index);
   }
+}
+
+//04. Editing function
+function editItemInArray(array, index) {
+  array.push(index);
+}
+
+//05. Counter function
+function countItemsInArray(array) {
+  return array.length;
 }
