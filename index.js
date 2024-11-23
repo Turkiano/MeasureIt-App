d89b746a6542dcbc8737621b341fcdc9fcb71dca
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-23 12:54:52 +0300

    Polymorphism is finito

--- index.js
+++ index.js
@@ -1,56 +1,32 @@
-class Animal {
-  constructor(name) {
-    this.name = name;
-  }
-  speak() {
-    return `${this.name} makes a sound.`;
-  }
-}
-
-class Dog extends Animal {
-  speak() {
-    return `${this.name} barks.`;
-  }
-}
-
-class Cat extends Animal {
-  speak() {
-    return `${this.name} mewos`;
-  }
-}
-
-const dog01 = new Dog("Max");
-console.log(dog01.speak()); // Max barks.
-
-const cat01 = new Cat("Becatcho");
-console.log(cat01.speak()); // Becatcho meows.
-
-
-
-//---------------The [super] keywrod--------------------
+//---------------The [Polymorphism ] example--------------------
 
 class Human {
-  constructor(name, age, createdAt) {
+  constructor(id, name, age, createdAt) {
+    this.id = id;
     this.name = name;
     this.age = age;
     this.createdAt = createdAt;
   }
+  getDetails() {
+    return `Name: ${this.name} Age: ${this.age}, `;
+  }
 
   register() {
-    return `${this.name} is registerd on ${this.createdAt}`;
+    return `This Id No: ${this.id}, is registerd on ${this.createdAt}`;
   }
 }
 
 class Employee extends Human {
-  constructor(name, age, createdAt, position) {
-    super(name, age, createdAt);
-    this.position = position
+  constructor(id, name, age, createdAt, position) {
+    super(id, name, age, createdAt);
+    this.position = position;
   }
-  getDetails(){
-    return `Age: ${this.age}, Position: ${this.position}`
+  getDetails() {
+    // Overriding the parent's method
+    return `Name: ${this.name} Age: ${this.age}, Position: ${this.position} `;
   }
 }
 
-const employee01 = new Employee("Turki", 38, "1987/08/10", "The Boss")
-console.log(employee01.register())
-console.log(employee01.getDetails())
+const employee01 = new Employee(1, "Turki", 38, "1987/08/10", "The Boss");
+console.log(employee01.register());
+console.log(employee01.getDetails());
