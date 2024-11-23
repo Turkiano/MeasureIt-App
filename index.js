b75ca178024662e5296ff7789704e1269f0798fb
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-23 14:44:40 +0300

    abstraction is finitio

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-//---------------The [Polymorphism ] example--------------------
-
-class Human {
-  constructor(id, name, age, createdAt) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.createdAt = createdAt;
+class BankAccount {
+  #blance; //Private field
+  constructor(initialBalance) {
+    this.#blance = initialBalance;
   }
-  getDetails() {
-    return `Name: ${this.name} Age: ${this.age}, `;
+  deposit(amount) {
+    if (amount > 0) this.#blance += amount;
   }
 
-  register() {
-    return `This Id No: ${this.id}, is registerd on ${this.createdAt}`;
+  getBalance() {
+    return this.#blance;
   }
 }
 
-class Employee extends Human {
-  constructor(id, name, age, createdAt, position) {
-    super(id, name, age, createdAt);
-    this.position = position;
+const account = new BankAccount(200);
+account.deposit(50);
+console.log(account.getBalance());
+
+
+console.log("----------------------Enhanced Abastraction Example------------------------")
+
+
+
+class BankAccount02 {
+  #balance;
+  constructor(initialBalance, testing) {
+    //validation
+    if (initialBalance < 0) throw new error("The total can't be negative.");
+    this.#balance = initialBalance;
+    this.testing = testing
+
   }
-  getDetails() {
-    // Overriding the parent's method
-    return `Name: ${this.name} Age: ${this.age}, Position: ${this.position} `;
+  deposit(cash) {
+    if (cash > 0) {
+      //validation
+      this.#balance += cash;
+    } else {
+      console.log("Deposit must be positive");
+    }
   }
+
+  withdraw(cash) {
+    if (cash > this.#balance) {
+      console.log("Insfficient funds.");
+    } else if (cash > 0) {
+      this.#balance -= cash;
+    } else {
+      console.log("must be a positve number");
+    }
+  }
+
+  getBalance() {
+    return this.#balance;
+  }
+
 }
 
-const employee01 = new Employee(1, "Turki", 38, "1987/08/10", "The Boss");
-console.log(employee01.register());
-console.log(employee01.getDetails());
+
+const account01 = new BankAccount(0);
+account01.deposit(-1);
+console.log(account01.getBalance());
