1422e580a19c78d170650ed2ff68a785060b8af9
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-23 11:17:44 +0300

    inheritance using the [super] keywrod

--- index.js
+++ index.js
@@ -7,22 +7,50 @@ class Animal {
   }
 }
 
-
-class Dog extends Animal{
-  speak(){
-    return `${this.name} barks.`
+class Dog extends Animal {
+  speak() {
+    return `${this.name} barks.`;
   }
 }
 
-class Cat extends Animal{
-  speak(){
-    return `${this.name} mewos`
+class Cat extends Animal {
+  speak() {
+    return `${this.name} mewos`;
   }
 }
-
 
 const dog01 = new Dog("Max");
-console.log(dog01.speak())// Max barks.
+console.log(dog01.speak()); // Max barks.
 
 const cat01 = new Cat("Becatcho");
-console.log(cat01.speak()) // Becatcho meows.
+console.log(cat01.speak()); // Becatcho meows.
+
+
+
+//---------------The [super] keywrod--------------------
+
+class Human {
+  constructor(name, age, createdAt) {
+    this.name = name;
+    this.age = age;
+    this.createdAt = createdAt;
+  }
+
+  register() {
+    return `${this.name} is registerd on ${this.createdAt}`;
+  }
+}
+
+class Employee extends Human {
+  constructor(name, age, createdAt, position) {
+    super(name, age, createdAt);
+    this.position = position
+  }
+  getDetails(){
+    return `Age: ${this.age}, Position: ${this.position}`
+  }
+}
+
+const employee01 = new Employee("Turki", 38, "1987/08/10", "The Boss")
+console.log(employee01.register())
+console.log(employee01.getDetails())
